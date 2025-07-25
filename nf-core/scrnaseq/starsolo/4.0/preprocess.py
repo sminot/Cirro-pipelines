b68305b488da3a3179fc62df78c852547e0ede19
Sam Minot <sminot@gmail.com>
2025-07-25 08:49:09 -0700

    User must select a GTF input as well

--- nf-core/scrnaseq/starsolo/4.0/preprocess.py
+++ nf-core/scrnaseq/starsolo/4.0/preprocess.py
@@ -29,7 +29,23 @@ samplesheet = (
 
 ds.logger.info("Formatted samplesheet:")
 ds.logger.info(samplesheet.to_csv(index=None))
-assert samplesheet.shape[0] > 0, "No files detected -- there may be an error with data ingest"
+assert samplesheet.shape[0] > 0, "No FASTQ files detected -- there may be an error with user selection or data ingest"
 
 # Write out to a file
 samplesheet.to_csv("samplesheet.csv", index=None)
+
+# Remove the "/SA" from the STAR index path
+if "star_index" in ds.params:
+    assert ds.params["star_index"].endswith("/SA"), "Expected STAR index path to end with '/SA'"
+    ds.add_param(
+        "star_index",
+        ds.params["star_index"].replace("/SA", ""),
+        overwrite=True
+    )
+    ds.logger.info(f"Updated STAR index path: {ds.params['star_index']}")
+else:
+    ds.logger.warning("No STAR index found in parameters, skipping update.")
+
+# Make sure that a "gtf" file is provided
+if "gtf" not in ds.params:
+    raise ValueError("No GTF file provided in parameters. Please ensure a GTF file is specified for the STAR alignment.")
