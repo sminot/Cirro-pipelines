259928e02ec8dbb34adec6cde9976aedd125a029
Sam Minot <sminot@gmail.com>
2025-10-27 11:43:26 -0700

    MADDD-seq: Specify inputs using samplesheet (#42)

---
+++ community/maddd-seq/1.0/preprocess.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from cirro.helpers.preprocess_dataset import PreprocessDataset
+
+ds = PreprocessDataset.from_running()
+
+ds.logger.info("Files annotated in the dataset:")
+ds.logger.info(ds.files.to_csv(index=None))
+
+# Filter out any index files that may have been uploaded
+ds.files = ds.files.loc[
+    ds.files.apply(
+        lambda r: r.get('readType', 'R') == 'R',
+        axis=1
+    )
+]
+
+# Make a wide samplesheet with the columns
+# sample, R1, R1
+samplesheet = (
+    ds.files
+    .reindex(columns=["dataset", "sampleIndex", "sample", "lane", "read", "file"])
+    .pivot(
+        index=["dataset", "sampleIndex", "sample", "lane"],
+        columns="read",
+        values="file"
+    )
+    .rename(columns=lambda i: f"R{int(i)}")
+    .rename(columns=dict(sample="specimen"))
+    .reset_index()
+    .reindex(columns=["sample", "R1", "R2"])
+)
+
+ds.logger.info("Formatted samplesheet:")
+ds.logger.info(samplesheet.to_csv(index=None))
+assert samplesheet.shape[0] > 0, "No files detected -- there may be an error with data ingest"
+
+# Write out to a file
+samplesheet.to_csv("samplesheet.csv", index=None)
+
+# log
+ds.logger.info(ds.params)
